3c4297d3f7616d02a9bf0285b482cb6d3b04c49c
chathura456 <chathuracde@gmail.com>
2023-02-24 13:53:52 +0530

    ui code added

---
+++ pill finder/Controllers/LoginController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pill_finder.Controllers
+{
+    public class LoginController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
