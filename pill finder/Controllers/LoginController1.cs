9465811e5f9b3556851185ee9008a31d47222f2d
chathura456 <chathuracde@gmail.com>
2023-02-24 15:44:31 +0530

    controller changes

--- pill finder/Controllers/LoginController1.cs
+++ pill finder/Controllers/LoginController1.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using pill_finder.Models;
 
 namespace pill_finder.Controllers
 {
+    
     public class LoginController1 : Controller
     {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Pharmacy Pharmacy { get; set; }
+
+        public LoginController1(ApplicationDbContext db)
+        {
+            _db= db;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Register() { 
+            Pharmacy = new Pharmacy();
+            return View(Pharmacy);
+
+        }
     }
+    
 }
