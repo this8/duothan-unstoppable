f370e580ca69d82163d1fed21e0844136f1399ab
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2023-02-24 11:31:40 +0530

    dotnet project initial commit

---
+++ pill finder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pill_finder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
